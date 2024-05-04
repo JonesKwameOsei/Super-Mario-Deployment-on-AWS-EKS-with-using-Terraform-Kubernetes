c4b38f76964677c0104f937c44f4fcc18319421f
JonesKwmaeOsei <oseikwamejones@gmail.com>
2024-05-04 13:03:12 +0100

    Updated keypair path in EC2.tf file

--- EC2.tf/variables.tf
+++ EC2.tf/variables.tf
@@ -10,6 +10,12 @@ variable "name" {
   default     = "Supermario_server"
 }
 
+variable "key_file_path" {
+  description = "Path to the public key file"
+  type        = string
+  default     = "id_rsa.pub" # Default value assuming the key file is in the same directory
+}
+
 variable "key_name" {
   type        = string
   description = "Name of the keypair to ssh into the instance"
