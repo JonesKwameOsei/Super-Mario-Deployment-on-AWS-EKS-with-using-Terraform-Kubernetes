2f668459e51c6d5b5f34d56dc7f4eb749eb296d9
JonesKwmaeOsei <oseikwamejones@gmail.com>
2024-05-04 11:56:35 +0100

    Adding configurations files and workflows

---
+++ EC2.tf/data.tf
@@ -0,0 +1,25 @@
+# EC2 instance data configurations
+# Inport const ec2InstanceDataConfigurations:
+// -------------------------------------------
+
+# Fetch the Latest Ubuntu AMI
+data "aws_ami" "ubuntu_latest" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "template_file" "userdata" {
+  template = file("${path.module}/userdata.tpl")
+}
+
+
