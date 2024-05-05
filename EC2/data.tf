0949e04ae57f1f270ab8e4b77e9799d8eca9fe4e
JonesKwmaeOsei <oseikwamejones@gmail.com>
2024-05-05 02:25:06 +0100

    updated config files

--- EC2/data.tf
+++ EC2/data.tf
@@ -1,29 +1,29 @@
-# # EC2 instance data configurations
-# # Inport const ec2InstanceDataConfigurations:
-# // -------------------------------------------
+# EC2 instance data configurations
+# Inport const ec2InstanceDataConfigurations:
+// -------------------------------------------
 
-# # Fetch the Latest Ubuntu AMI
-# data "aws_ami" "ubuntu_latest" {
-#   most_recent = true
-#   owners      = ["099720109477"] # Canonical
+# Fetch the Latest Ubuntu AMI
+data "aws_ami" "ubuntu_latest" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
 
-#   filter {
-#     name   = "name"
-#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-#   }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# data "template_file" "userdata" {
+#   template = file("${path.module}/userdata.tpl")
 # }
 
-# # data "template_file" "userdata" {
-# #   template = file("${path.module}/userdata.tpl")
-# # }
-
-# data "aws_key_pair" "mario_keypair" {
-#   key_name           = "MyMarioKeyPair"
-# }
+data "aws_key_pair" "mario_keypair" {
+  key_name           = "MyMarioKeyPair"
+}
 
 
