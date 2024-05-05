d6daf1fc5a22be935329419ead4f645171f7f8af
JonesKwmaeOsei <oseikwamejones@gmail.com>
2024-05-05 02:05:58 +0100

    destroy infra

--- EC2/output.tf
+++ EC2/output.tf
@@ -1,7 +1,7 @@
-output "instance_ip_addr" {
-  value = aws_instance.supermario_server.public_ip
-}
+# output "instance_ip_addr" {
+#   value = aws_instance.supermario_server.public_ip
+# }
 
-output "name" {
-  value = data.aws_key_pair.mario_keypair.key_name
-}
+# output "name" {
+#   value = data.aws_key_pair.mario_keypair.key_name
+# }
